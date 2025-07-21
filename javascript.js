147024339fa73e61b70d697b2b1f9787fe6eafa2
Iris Xu <xuiris142@gmail.com>
2025-07-21 17:34:00 -0600

    create 16*16 square divs inside container div in javascript.js

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16*16; i++){
+    const square = document.createElement("div");
+    square.classList.add('grid-square');
+    container.appendChild(square);
+
+}
