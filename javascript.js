4013dc478aa11e0c9afe9f2f787b64ce09d6a63e
Iris Xu <xuiris142@gmail.com>
2025-07-21 21:55:43 -0600

    edit css and js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,25 @@
 const container = document.querySelector("#container");
+const body = document.querySelector("body");
 
-for (let i = 0; i < 16*16; i++){
-    const square = document.createElement("div");
-    square.classList.add('grid-square');
-    container.appendChild(square);
+function createGrid(n) {
 
+    for (let i = 0; i < n*n; i++){
+        const square = document.createElement("div");
+        square.classList.add('grid-square');
+        square.style.width = `${100/n}%`;
+        square.style.height = `${100/n}%`;
+        container.appendChild(square);
+
+    }
 }
+
+const size = document.createElement("button");
+size.textContent = "Choose Size";
+
+let n = 0;
+size.addEventListener("click", () => {
+    n = prompt("Enter a number between 0 and 100.");
+    createGrid(n);
+})
+
+body.insertBefore(size, container);
